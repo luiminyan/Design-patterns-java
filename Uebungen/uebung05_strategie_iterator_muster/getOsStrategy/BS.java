8dae21e3018a3a766f4be2cd5995f11ad5e9cccc
luiminyan <luiminyan@gmail.com>
2023-08-27 21:59:32 +0200

    correction

--- Uebungen/uebung05_strategie_iterator_muster/getOsStrategy/BS.java
+++ Uebungen/uebung05_strategie_iterator_muster/getOsStrategy/BS.java
@@ -4,14 +4,27 @@ public class BS {
     private Strategie strategie;
 
     // constructor
+    public BS() {
+        String os = System.getProperty("os.name");
+        if (os.startsWith("Windows")) {
+            strategie = new Windowsstrategie();
+        }
+        else if (os.startsWith("Linux")) {
+            strategie = new Linuxstrategie();
+        }
+        else if (os.startsWith("Mac")) {
+            strategie = new MacStrategy();
+        }
+        else {
+            strategie = null;
+            System.out.println("Betriebssystem is nicht bekannt");
+        }
+    }
 
-    String os = System.getProperty("os.name");
-    
-    boolean isWin = os.startsWith("Windows");
-    boolean isLinux = os.startsWith("Linux");
-    boolean isMac = os.startsWith("Mac");
-
-    
+    //getOS
+    public String getOS() {
+        return strategie.getOS();
+    }
 }
 
 
--- Uebungen/uebung05_strategie_iterator_muster/getOsStrategy/OSTest.java
+++ Uebungen/uebung05_strategie_iterator_muster/getOsStrategy/OSTest.java
@@ -3,8 +3,6 @@ package getOsStrategy;
 public class OSTest {
     public static void main(String[] args) {
        BS bs = new BS();
-       // System.out.println(bs);
-
-       System.out.println("Hello!");
+       System.out.println(bs.getOS());
     }
 }
